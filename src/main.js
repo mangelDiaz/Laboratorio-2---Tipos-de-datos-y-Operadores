32725df9200861f7c12360eea52711a928ae961d
Miguel Ángel Díaz <mangeldiaz82@gmail.com>
2026-07-01 00:57:25 +0200

    laboratorio 2 terminado

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-import "./style.css";
+/* Cena de cumpleaños entre amigos
+Tienes un grupo de 6 amigos y quieres invitarlos a cenar por tu cumpleaños.
 
-console.log("Hello from main");
+Solo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero.
+Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €.
+Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa.
+Utiliza JavaScript para hacer el cálculo y mostrar el resultado por consola. */
+
+
+console.log((120 - 18) / 6);
